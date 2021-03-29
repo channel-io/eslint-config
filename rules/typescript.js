5728e1afd46582bfce0f9354d0dffe9dfa0ce67d
Hakyeon Manuel Kim <khy@aooen.com>
2021-03-29 20:06:16 +0900

    Apply new channel.io eslint plugin rule (#17)
    * Apply new channel.io eslint plugin rule
    
    * pascal-case-enum-name에 대한 option 정의 (member는 pascal case가 아닐 수 있음)
    
    * Update to 1.1.1

--- rules/typescript.js
+++ rules/typescript.js
@@ -3,4 +3,7 @@ module.exports = {
   'no-dupe-class-members': 'off', // Allow method overriding (will be checked by tsc)
   'no-undef': 'off',
   'no-unused-vars': 'off',
+  '@channel.io/pascal-case-enum-name': ['error', true, false],
+  '@channel.io/pascal-case-interface-name': ['error'],
+  '@channel.io/pascal-case-type-name': ['error'],
 }
