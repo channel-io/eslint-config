9742e8f405ccbd137927bef40099f8a3f3d73d80
Dino (한도협) <dinohan.dev@gmail.com>
2023-02-28 14:17:05 +0900

    fix(rules): @channel.io/no-translate-with-template-literal 을 warn이 아닌 error로 수정 (#72)

--- rules/base.js
+++ rules/base.js
@@ -41,7 +41,7 @@ module.exports = {
   'no-param-reassign': ['error', { props: false }],
   'no-restricted-imports': ['error', { paths: ['src'], patterns: ['../*'], }],
   'no-restricted-modules': ['error', { paths: ['src'], patterns: ['../*'], }],
-  '@channel.io/no-translate-with-template-literal': 'warn',
+  '@channel.io/no-translate-with-template-literal': 'error',
   'no-underscore-dangle': 'off',
   'no-unused-expressions': 'off', // use babel/no-unused-expressions
   'no-unused-vars': ['error', { ignoreRestSiblings: true }],
